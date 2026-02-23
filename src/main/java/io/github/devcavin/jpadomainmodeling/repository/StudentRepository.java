4f07032b07724e49f33226d9a11830fdcb61f7b3
Cavin <cavinlarry001@gmail.com>
2026-02-23 23:12:06 +0300

    Repo rollover

--- src/main/java/io/github/devcavin/jpadomainmodeling/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.devcavin.jpadomainmodeling.repository;
-
-import io.github.devcavin.jpadomainmodeling.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
